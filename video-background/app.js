3632fbfd0afff1d3946bbe7ea61c99e924e50407
Archana Kodange <kodange_archana@yahoo.com>
2021-07-22 19:33:51 +0530

    Add Cafe Website Landing Page

--- video-background/app.js
+++ video-background/app.js
@@ -1,1 +1,22 @@
-﻿
+﻿const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+});
+
+const video = document.getElementById('bgvideo');
+const control = document.querySelector('.control');
+
+control.addEventListener('click', () => {
+    control.classList.toggle('play');
+    if (video.paused) {
+        video.play();
+        control.title = "pause";
+    }
+    else {
+        video.pause();
+        control.title = "play";
+    }
+});
